97e5211c7590c3e3304260eb4cf4977ecbd9d8ab
GeorgeContreras241 <maicolcontreras241@gmail.com>
2024-11-18 10:24:05 -0500

    Correcion estilos Css

--- Components/Table.jsx
+++ Components/Table.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export const Table = ({handleDelate, color, handleOrderName, handleOrderLast, renderFilter}) => {
+export const Table = ({handleDelate, color, handleOrderName, handleOrderLast,handleOrderCountry, renderFilter}) => {
     return (
         <table>
             <thead>
-                <tr>
-                    <th>Foto</th>
+                <tr className='header'>
+                    <td>Foto</td>
                     <th onClick={handleOrderName}>Nombre</th>
                     <th onClick={handleOrderLast}>Apellido</th>
-                    <th>País</th>
+                    <th onClick={handleOrderCountry}>País</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
